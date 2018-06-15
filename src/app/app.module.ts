b8d4d2119d8a8d20ffab2f3e7ae346f7ea5fe7bd
saurabh <saurabhyadav4321@gmail.com>
2018-06-15 21:00:58 +0530

    sidebar custom scroller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ScrollbarModule } from 'ngx-scrollbar';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -17,6 +17,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
+    ScrollbarModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/components/grids/grids.component.ts
+++ src/app/components/grids/grids.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grids',
   templateUrl: './grids.component.html',
   styleUrls: ['./grids.component.scss']
 })
-export class GridsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class GridsComponent { }
