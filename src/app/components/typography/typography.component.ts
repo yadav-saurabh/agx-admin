67a48fde9e37ffd07a3aac59baa92677d0a3b0c4
saurabh <saurabhyadav4321@gmail.com>
2018-06-19 17:41:20 +0530

    got rid of extra code

--- src/app/components/typography/typography.component.ts
+++ src/app/components/typography/typography.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-typography',
   templateUrl: './typography.component.html',
-  styleUrls: ['./typography.component.scss']
+  styles: ['.note{font-size: 15px;}']
 })
 export class TypographyComponent {
 
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,5 +9,27 @@ export class SidebarComponent {
 
   @Input()
   sidebarToggle;
-
+  sidebarItems = [
+    {link:'/',label:'dashboard',icon:'dashboard'},
+    {label:'components',icon:'apps',subItem:[
+      {link:'/components/buttons',label:'buttons',icon:'b'},
+      {link:'/components/grids',label:'grid System',icon:'gs'},
+      {link:'/components/panels',label:'panels',icon:'p'},
+      {link:'/components/alerts',label:'alerts',icon:'a'},
+      {link:'/components/notifications',label:'notifications',icon:'n'},
+      {link:'/components/icons',label:'icons',icon:'i'},
+      {link:'/components/typography',label:'typography',icon:'t'},
+    ]},
+    {label:'forms',icon:'ballot', subItem:[
+      {link:'/forms/elements',label:'Form Elemets',icon:'fe'},
+      {link:'/forms/validations',label:'Form Validation',icon:'fv'},
+      {link:'/forms/examples',label:'Form Examples',icon:'se'}
+    ]},
+    {link:'#', label:'tables',icon:'grid_on'}, 
+    {link:'#', label:'charts',icon:'show_chart'}, 
+    {link:'#', label:'maps',icon:'place'}, 
+    {link:'#', label:'editors',icon:'edit'}, 
+    {link:'#', label:'pages',icon:'pages'}, 
+    {link:'#', label:'calendar',icon:'date_range'} 
+  ];
 }
