47a336ddeb33caca86c91b6200c5b709c73920f0
saurabh <saurabhyadav4321@gmail.com>
2018-07-01 17:34:00 +0530

    added progress bar on lazy module loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
+import * as NProgress from 'nprogress';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,25 @@ export class AppComponent implements OnInit {
 
   closeSidebar = true;
   pagesRoute = false;
-  
-  constructor(private router: Router){  }
-  
+
+  constructor(private router: Router) { }
+
   ngOnInit() {
-    this.router.events.subscribe((obj:any)=>{
-      this.pagesRoute = (!!obj.url && obj.url.includes('/pages'))?true:false;
+    this.router.events.subscribe((obj: any) => {
+      this.pagesRoute = (!!obj.url && obj.url.includes('/pages')) ? true : false;
+      if (obj instanceof RouteConfigLoadStart) {
+        NProgress.start();
+        NProgress.set(0.4);
+      } else if (obj instanceof RouteConfigLoadEnd) {
+        NProgress.set(0.9);
+        setTimeout(() => {
+          NProgress.done();
+          NProgress.remove();
+        }, 500);
+      }
     });
   }
-  
+
   navBarEvent(event) {
     this.closeSidebar = event;
   }
