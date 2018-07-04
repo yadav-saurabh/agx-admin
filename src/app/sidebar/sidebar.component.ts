c457f52ce25db56081c62e4d861f48627cc1a6f3
saurabh <saurabhyadav4321@gmail.com>
2018-07-04 21:31:18 +0530

    responsive for md screen

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,8 +11,8 @@ export class SidebarComponent {
   constructor(private cs:CommonService){}
 
   sidebarItems = [
-    {link:'/',label:'dashboard',icon:'dashboard'},
-    {label:'components',icon:'apps',subItem:[
+    {link:'/',label:'Dashboard',icon:'dashboard'},
+    {label:'Components',icon:'apps',subItem:[
       {link:'/components/buttons',label:'buttons',icon:'b'},
       {link:'/components/grids',label:'grid System',icon:'gs'},
       {link:'/components/panels',label:'panels',icon:'p'},
@@ -21,20 +21,20 @@ export class SidebarComponent {
       {link:'/components/icons',label:'icons',icon:'i'},
       {link:'/components/typography',label:'typography',icon:'t'},
     ]},
-    {label:'forms',icon:'ballot', subItem:[
+    {label:'Forms',icon:'ballot', subItem:[
       {link:'/forms/elements',label:'Form Elemets',icon:'fe'},
       {link:'/forms/validations',label:'Form Validation',icon:'fv'},
       {link:'/forms/examples',label:'Form Examples',icon:'se'}
     ]},
-    {label:'pages',icon:'pages',subItem:[
+    {label:'Pages',icon:'pages',subItem:[
       {link:'/pages/notfound',label:'Not Found',icon:'nf'},
       {link:'/pages/auth',label:'Auth',icon:'A'}
     ]}, 
-    {link:'/tables', label:'tables',icon:'grid_on'}, 
-    {link:'/charts', label:'charts',icon:'show_chart'}, 
-    {link:'/maps', label:'maps',icon:'place'}, 
-    {link:'/editors', label:'editors',icon:'edit'}, 
-    {link:'/calendar', label:'calendar',icon:'date_range'} 
+    {link:'/tables', label:'Tables',icon:'grid_on'}, 
+    {link:'/charts', label:'Charts',icon:'show_chart'}, 
+    {link:'/maps', label:'Maps',icon:'place'}, 
+    {link:'/editors', label:'Editors',icon:'edit'}, 
+    {link:'/calendar', label:'Calendar',icon:'date_range'} 
   ];
 
 }
