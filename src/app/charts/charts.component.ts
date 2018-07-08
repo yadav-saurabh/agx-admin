5ec3ac3089d22fd1a1334d89b7ce07c7d3464cdb
saurabh <saurabhyadav4321@gmail.com>
2018-07-08 11:29:49 +0530

    charts done

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,73 +1,313 @@
 import { Component, OnInit } from '@angular/core';
-declare var Chart: any;
+import * as Chart from 'chart.js'
 
 @Component({
   selector: 'app-charts',
-  templateUrl: './charts.component.html',
-  styleUrls: ['./charts.component.scss']
+  templateUrl: './charts.component.html'
 })
 export class ChartsComponent implements OnInit {
 
-  data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  };
-
-  constructor() {}
-
+  exampleChartType = 'line';
+  exampleChart;
+  exampleDataSet1;
+  exampleDataSet2;
+  exampleData;
+  exampleChartCanvas;
+  
+  constructor() { }
+  
   ngOnInit() {
-    const pie = document.getElementById("pieChart");
-    const polar = document.getElementById("polarChart");
-    const radar = document.getElementById("radarChart");
-    const bar = document.getElementById("barChart");
-    let pieChart = new Chart(pie, {
-      type: 'pie',
-      data: this.data,
-      options: {
-        responsive: true
-      }
-    });
-    let polarChart = new Chart(polar, {
-      type: 'polarArea',
-      data: this.data,
-      options: {
-        responsive: true
-      }
-    });
-    let radarChart = new Chart(radar, {
+    //example
+    this.setExample();
+    // Bar chart
+    this.barChart();
+    //Line Chart
+    this.lineChart();
+    //Doughnut Chart
+    this.doughnutChart();
+    //Mixed Chart
+    this.mixedChart();
+    //Bubble Chart
+    this.bubbleChart();
+    //Radar Chart
+    this.radarChart();
+  }
+
+  private radarChart() {
+    new Chart(document.getElementById("radar-chart"), {
       type: 'radar',
-      data: this.data,
+      data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+          {
+            label: "1950",
+            fill: true,
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            data: [8.77, 55.61, 21.69, 6.62, 6.82]
+          }, {
+            label: "2050",
+            fill: true,
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            pointBorderColor: "#fff",
+            pointBackgroundColor: "rgba(255,99,132,1)",
+            data: [25.48, 54.16, 7.61, 8.06, 4.45]
+          }
+        ]
+      },
       options: {
-        responsive: true
-      }
-    });
-    let barChart = new Chart(bar, {
-      type: 'horizontalBar',
-      data: this.data,
-      options: {
-        responsive: true
+        title: {
+          display: true,
+          text: 'Distribution in % of world population'
+        }
       }
     });
   }
+
+  private bubbleChart() {
+    new Chart(document.getElementById("bubble-chart"), {
+      type: 'bubble',
+      data: {
+        labels: "Africa",
+        datasets: [
+          {
+            label: ["China"],
+            backgroundColor: "rgba(255,221,50,0.2)",
+            borderColor: "rgba(255,221,50,1)",
+            data: [{
+              x: 21269017,
+              y: 5.245,
+              r: 15
+            }]
+          }, {
+            label: ["Denmark"],
+            backgroundColor: "rgba(60,186,159,0.2)",
+            borderColor: "rgba(60,186,159,1)",
+            data: [{
+              x: 258702,
+              y: 7.526,
+              r: 10
+            }]
+          }, {
+            label: ["Germany"],
+            backgroundColor: "rgba(0,0,0,0.2)",
+            borderColor: "#000",
+            data: [{
+              x: 3979083,
+              y: 6.994,
+              r: 15
+            }]
+          }, {
+            label: ["Japan"],
+            backgroundColor: "rgba(193,46,12,0.2)",
+            borderColor: "rgba(193,46,12,1)",
+            data: [{
+              x: 4931877,
+              y: 5.921,
+              r: 15
+            }]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Predicted world population (millions) in 2050'
+        },
+        scales: {
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: "Happiness"
+            }
+          }],
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: "GDP (PPP)"
+            }
+          }]
+        }
+      }
+    });
+  }
+
+  private mixedChart() {
+    new Chart(document.getElementById("mixed-chart"), {
+      type: 'bar',
+      data: {
+        labels: ["1900", "1950", "1999", "2050"],
+        datasets: [{
+          label: "Europe",
+          type: "line",
+          borderColor: "#8e5ea2",
+          data: [408, 547, 675, 734],
+          fill: false
+        }, {
+          label: "Africa",
+          type: "line",
+          borderColor: "#3e95cd",
+          data: [133, 221, 783, 2478],
+          fill: false
+        }, {
+          label: "Europe",
+          type: "bar",
+          backgroundColor: "rgba(0,0,0,0.2)",
+          data: [408, 547, 675, 734],
+        }, {
+          label: "Africa",
+          type: "bar",
+          backgroundColor: "rgba(0,0,0,0.2)",
+          backgroundColorHover: "#3e95cd",
+          data: [133, 221, 783, 2478]
+        }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Population growth (millions): Europe & Africa'
+        },
+        legend: { display: false }
+      }
+    });
+  }
+
+  private doughnutChart() {
+    new Chart(document.getElementById("doughnut-chart"), {
+      type: 'doughnut',
+      data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+            data: [2478, 5267, 734, 784, 433]
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Predicted world population (millions) in 2050'
+        }
+      }
+    });
+  }
+
+  private lineChart() {
+    new Chart(document.getElementById("line-chart"), {
+      type: 'line',
+      data: {
+        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+        datasets: [{
+          data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+          label: "Africa",
+          borderColor: "#3e95cd",
+          fill: false
+        }, {
+          data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+          label: "Asia",
+          borderColor: "#8e5ea2",
+          fill: false
+        }, {
+          data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+          label: "Europe",
+          borderColor: "#3cba9f",
+          fill: false
+        }, {
+          data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+          label: "Latin America",
+          borderColor: "#e8c3b9",
+          fill: false
+        }, {
+          data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+          label: "North America",
+          borderColor: "#c45850",
+          fill: false
+        }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'World population per region (in millions)'
+        }
+      }
+    });
+  }
+
+  private barChart() {
+    new Chart(document.getElementById("bar-chart"), {
+      type: 'bar',
+      data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+            data: [2478, 5267, 734, 784, 433]
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+        title: {
+          display: true,
+          text: 'Predicted world population (millions) in 2050'
+        }
+      }
+    });
+  }
+
+  private setExample() {
+    // Define data set for all charts
+    this.exampleDataSet1 = [1, 10, 5, 2, 20, 30, 45];
+    this.exampleDataSet2 = [20, 30, 15, 12, 21, 30, 40];
+    this.exampleData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "DataSet1!",
+          fill: false,
+          backgroundColor: 'rgb(111, 110, 235, 0.5)',
+          borderColor: 'rgb(111, 110, 235)',
+          data: this.exampleDataSet1,
+        },
+        {
+          label: "DataSet2!",
+          fill: true,
+          backgroundColor: 'rgba(170, 102, 204, 0.5)',
+          borderColor: 'rgb(170, 102, 204)',
+          data: this.exampleDataSet2,
+        }]
+    };
+    // Default chart defined with type: 'line'
+    this.exampleChartCanvas = document.getElementById('exampleChart');
+    this.exampleChart = new Chart(this.exampleChartCanvas, {
+      type: this.exampleChartType,
+      data: this.exampleData
+    });
+  }
+  // Function runs on chart type select update
+  updateChartType() {
+    // Since you can't update chart type directly in Charts JS you must destroy original chart and rebuild
+    this.exampleChart.destroy();
+    this.exampleChart = new Chart(this.exampleChartCanvas, {
+      type: this.exampleChartType,
+      data: this.exampleData,
+    });
+  };
+  // Randomize data button function
+  randomizeData() {
+    let newDataBaby = this.exampleDataSet1.map(x => Math.floor(Math.random() * 50));
+    let newdataSet2Baby = this.exampleDataSet2.map(x => Math.floor(Math.random() * 40));
+    this.exampleData.datasets[0].data = newDataBaby
+    this.exampleData.datasets[1].data = newdataSet2Baby
+    this.exampleChart.update();
+  };
 
 }
--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsComponent } from './charts.component';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 const routes: Routes = [
   { path: '', component: ChartsComponent }
@@ -11,6 +13,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    NgSelectModule,
     RouterModule.forChild(routes)
   ],
   declarations: [ChartsComponent]
