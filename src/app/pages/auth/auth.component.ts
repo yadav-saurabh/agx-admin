a84598018af8345e7f1250cd106d7980b4a16964
saurabh <saurabhyadav4321@gmail.com>
2018-07-09 02:10:41 +0530

    auth

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -7,31 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+ toggleFormClass;
+  
   constructor() { }
 
-  ngOnInit() {
-    /**
- * Variables
- */
-    const signupButton = document.getElementById('signup-button'),
-      loginButton = document.getElementById('login-button'),
-      userForms = document.getElementById('user_options-forms')
-
-    /**
-    * Add event listener to the "Sign Up" button
-    */
-    signupButton.addEventListener('click', () => {
-      userForms.classList.remove('bounceRight')
-      userForms.classList.add('bounceLeft')
-    }, false)
-
-    /**
-    * Add event listener to the "Login" button
-    */
-    loginButton.addEventListener('click', () => {
-      userForms.classList.remove('bounceLeft')
-      userForms.classList.add('bounceRight')
-    }, false)
+  ngOnInit() {  }
+  
+  showSignUp(){
+    this.toggleFormClass = 'bounceLeft';
+  }
+  
+  showLogin(){
+    this.toggleFormClass = 'bounceRight';
   }
 
 }
