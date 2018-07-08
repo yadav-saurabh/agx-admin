b08bdd863c4089624a3614f508a90e634e07e510
saurabh <saurabhyadav4321@gmail.com>
2018-07-08 19:51:03 +0530

    calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -12,10 +12,77 @@ export class CalendarComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit() {
+    this.monthCal();
+    this.weekCal();
+    this.eventCal();
+  }
+
+
+  private eventCal() {
     $('#calendar').fullCalendar({
-      // put your options and callbacks here
-    })
+      themeSystem: 'bootstrap4',
+      defaultView: $(window).width() < 765 ? 'listWeek' : 'month',
+      header: {
+        left: '',
+        center: 'prev title next',
+        right: 'today '
+      },
+      footer: {
+        center: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      titleFormat: 'MMM YYYY',
+      buttonText: {
+        today: ' '
+      },
+      defaultDate: '2016-08-06',
+      editable: true,
+      eventLimit: true,
+      events: [
+        { title: 'All Day Event', start: '2016-08-01' },
+        { title: 'Long Event', start: '2016-08-07', end: '2016-08-10' },
+        { id: 999, title: 'Repeating Event', start: '2016-08-09T16:00:00' },
+        { id: 999, title: 'Repeating Event', start: '2016-08-16T16:00:00' },
+        { title: 'Conference', start: '2016-08-05', end: '2016-08-07' },
+        { title: 'Meeting', start: '2016-08-06T10:30:00', end: '2016-08-06T12:30:00' },
+        { title: 'Lunch', start: '2016-08-06T12:00:00' },
+        { title: 'Meeting', start: '2016-08-06T14:30:00' },
+        { title: 'Happy Hour', start: '2016-08-06T17:30:00' },
+        { title: 'Dinner', start: '2016-08-06T20:00:00' },
+        { title: 'Movie', start: '2016-08-07T07:00:00' },
+        { title: 'Click for Google', url: 'https://google.com/', start: '2016-08-28' }
+      ]
+    });
+  }
+
+  private weekCal() {
+    $('#calendar-week').fullCalendar({
+      defaultView: 'basicWeek',
+      themeSystem: 'bootstrap4',
+      header: {
+        left: '',
+        center: 'prev title next',
+        right: 'today '
+      },
+      buttonText: {
+        today: ' '
+      }
+    });
+  }
+
+  private monthCal() {
+    $('#calendar-month').fullCalendar({
+      themeSystem: 'bootstrap4',
+      header: {
+        left: '',
+        center: 'prev title next',
+        right: 'today '
+      },
+      buttonText: {
+        today: ' '
+      }
+    });
   }
 
 }
