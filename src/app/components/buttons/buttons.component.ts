47c2997ab6874a60159bfebea6e649b3d549534f
yadav-saurabh <yadav-saurabh@outlook.com>
2018-08-29 10:44:24 +0530

    pagnation button changes

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.scss']
 })
-export class ButtonsComponent { }
+export class ButtonsComponent {
+  paginationPage = 1;
+
+  incPage() {
+    if (this.paginationPage < 5) { this.paginationPage++; }
+  }
+
+  decPage() {
+    if (this.paginationPage > 1) {
+      this.paginationPage--;
+    }
+  }
+
+  goToPage(number) {
+    this.paginationPage = number;
+  }
+}
